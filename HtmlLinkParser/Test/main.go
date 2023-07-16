40638a9080b382d23882fcea9397cd30428e69cb
Pranav N <pranavbharadwaj142@gmail.com>
2023-07-16 12:35:16 +0530

    main.go

---
+++ HtmlLinkParser/Test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/htmlparser/link"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
+
+func main() {
+	fmt.Println("example11")
+	r := strings.NewReader(exampleHtml)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
